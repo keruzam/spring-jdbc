c9d56a3d09307bc888698e58adaffc1f24ed801b
keruzam <mazurek.zg@gmail.com>
2024-10-20 23:50:47 +0200

    H2 data source settings

--- src/main/java/pl/keruzam/spring_jdbc/SpringJdbcApplication.java
+++ src/main/java/pl/keruzam/spring_jdbc/SpringJdbcApplication.java
@@ -1,13 +1,32 @@
 package pl.keruzam.spring_jdbc;
 
+import lombok.extern.java.Log;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.jdbc.core.JdbcTemplate;
+
+import javax.sql.DataSource;
 
 @SpringBootApplication
-public class SpringJdbcApplication {
+@Log
+public class SpringJdbcApplication implements CommandLineRunner {
+
+	DataSource dataSource;
+
+	public SpringJdbcApplication(DataSource dataSource) {
+		this.dataSource = dataSource;
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(SpringJdbcApplication.class, args);
 	}
 
+	@Override
+	public void run(String... args) throws Exception {
+		log.info("check console");
+		log.info("DataSource: " + dataSource.toString());
+		JdbcTemplate jdbcTemplate = new JdbcTemplate(dataSource);
+		jdbcTemplate.execute("select 1");
+	}
 }
